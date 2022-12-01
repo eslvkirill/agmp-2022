8cd78d9bfd3f6146182cb2d618e385b3881df2ac
Kirill Eselev <kirill_eselev@epam.com>
2022-12-01 17:38:49 +0400

    removed unnecessary code

--- video-courses/src/app/shared/directives/if-autentificated/if-autentificated.directive.ts
+++ video-courses/src/app/shared/directives/if-autentificated/if-autentificated.directive.ts
@@ -1,13 +1,4 @@
-import {
-  Directive,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-  TemplateRef,
-  ViewContainerRef,
-} from '@angular/core';
-import { AuthService } from '../../../shell/header/services/auth.service';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appIfAutentificated]',
@@ -16,27 +7,7 @@ export class IfAutentificatedDirective {
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef
-  ) // private authService: AuthService
-  {}
-
-  // ngOnInit(): void {
-  //   this.isAuthenticated = this.authService.isAuthenticated();
-  //   console.log(this.isAuthenticated);
-  // }
-
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   this.setAuth();
-  // }
-
-  // private setAuth(): void {
-  //   console.log(this.isAuthenticated);
-
-  //   if (this.isAuthenticated) {
-  //     this.viewContainer.createEmbeddedView(this.templateRef);
-  //     return;
-  //   }
-  //   this.viewContainer.clear();
-  // }
+  ) {}
 
   @Input('appIfAutentificated') set isAuthenticated(isAuthenticated: boolean) {
     if (isAuthenticated) {
